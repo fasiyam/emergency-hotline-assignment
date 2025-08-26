f90b6432d8f423fb73b8e599a24dc5ecf39129f2
fasiyam <famimahmedsiyam@gmail.com>
2025-08-26 23:49:59 +0600

    minor adjustment in responsive in mobile device

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'custom-figma': '0px 2px 3px 0px rgba(0, 0, 0, 0.15)',
+      },
+    },
+  },
+  plugins: [],
+}
